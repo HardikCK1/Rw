f877926ad2a92ed7007585b3d6541e246cc0692b
HardikCK1 <102613001+HardikCK1@users.noreply.github.com>
2025-01-11 15:10:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Optionally add interactivity such as event listeners here
+    // Optional JS interactivity for multiple pages.
+    const events = document.querySelectorAll(".event");
+    events.forEach(event => {
+        event.addEventListener("mouseover", () => {
+            event.innerHTML = "<strong>" + event.getAttribute("data-content") + "</strong>";
+        });
+        event.addEventListener("mouseout", () => {
+            event.innerHTML = event.querySelector("h3").innerText;
+        });
+    });
 });
